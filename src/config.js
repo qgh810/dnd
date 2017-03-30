de5e54431e69735fca47a0c9855045c597a7b31b
邱国辉 <530784837@qq.com>
2017-03-30 13:50:28 +0800

    程序中添加项目说明文档地址

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const DOCUMENT_ADDR = ''
+export const DOCUMENT_ADDR = 'https://github.com/qgh810/dnd'
 export const REMOVE_ANIMATION_TYPES = {
   'fade': 'animation_fade',
   'blost': 'animation_blost',
