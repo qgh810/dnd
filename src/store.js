d1608c832ece5011dbd53872e4c78ba073d9b191
邱国辉 <530784837@qq.com>
2017-06-19 15:32:15 +0800

    去掉打印信息

--- src/store.js
+++ src/store.js
@@ -166,8 +166,6 @@ const dragStore = {
       }
     }
 
-    console.log('目标索引: ', targetIndex)
-    console.log('目标位置: ', this.targetPositions)
     return targetIndex
   },
 
@@ -274,16 +272,13 @@ const dragStore = {
    * name 实例名称
    */
   destroyDrop (name) {
-    console.log('dnd销毁: ', name)
     let result = false
-    console.log(this.targets)
     this.targets.forEach((item, i) => {
       if (item.name === name) {
         this.removeDrop(i)
         result = true
       }
     })
-    console.log('dnd销毁结果: ', result)
     return result
   },
 
